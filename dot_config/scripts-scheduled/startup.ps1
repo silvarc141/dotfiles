05bea2787cf0083a8f3b93a4ad3b05be0eab3c68
Marta Karaniewicz-Biedna <marta.karaniewicz.biedna@gmail.com>
2023-12-19 15:11:54 +0100

    Update .config/scoop/config.json Update .config/scripts-scheduled/startup.ps1

--- dot_config/scripts-scheduled/startup.ps1
+++ dot_config/scripts-scheduled/startup.ps1
@@ -1,6 +1,5 @@
-. $env:USERPROFILE\.config\scripts-commands\hide-dot-files-in-directory.ps1 $env:USERPROFILE
-
 Start-Process -FilePath "$env:USERPROFILE\.config\autohotkey\de-elevated-run-init.ahk"
+Start-Process -FilePath "$env:PROGRAMFILES\1Password\app\8\1Password.exe" -ArgumentList "--silent"
 
 $ScoopAppsPath = "$env:USERPROFILE\scoop\apps"
 Start-Process -FilePath "$ScoopAppsPath\micaforeveryone\current\MicaForEveryone.exe"
@@ -10,3 +9,5 @@ Start-Process -FilePath "$ScoopAppsPath\fluent-search\current\FluentSearch.exe"
 #Start-Process -FilePath "$ScoopAppsPath\roundedtb\current\RoundedTB.exe"
 #Start-Process -FilePath "$ScoopAppsPath\translucenttb\current\TranslucentTB.exe"
 #Start-Process -FilePath "$ScoopAppsPath\eartrumpet\current\EarTrumpet.exe"
+
+. $env:USERPROFILE\.config\scripts-commands\hide-dot-files-in-directory.ps1 $env:USERPROFILE
