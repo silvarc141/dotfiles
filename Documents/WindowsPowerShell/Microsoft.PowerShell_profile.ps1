36c2aeb126ea6e0ebfee750e3e67f7d9823633ae
silvarc <silvarc@tutanota.com>
2024-01-16 01:48:30 +0100

    Update AppData/Local/1Password/settings/settings.json Update Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1 Update .config/scoop/config.json Update scoop/persist/keypirinha/portable/Profile/User/PackageControl.ini Update scoop/persist/sharex/ShareX/ApplicationConfig.json Update scoop/persist/sharex/ShareX/HotkeysConfig.json

--- Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -14,14 +14,8 @@ function Invoke-Starship-TransientFunction {
 }
 
 Invoke-Expression (&starship init powershell)
-#Enable-TransientPrompt
 
 # oh-my-posh --init --shell pwsh --config C:\| Invoke-Expression
 # oh-my-posh --init --shell pwsh | Invoke-Expression
 
-#TODO find a better way to pass arguments (maybe Invoke-Expression)
-function Dotfiles-Bare-Git{
-    & $env:USERPROFILE\scoop\apps\git\current\cmd\git.exe --git-dir=$env:USERPROFILE\.cfg\ --work-tree=$env:USERPROFILE $Args[0] $Args[1] $Args[2] $Args[3] $Args[4] $Args[5]
-}
-
-Set-Alias -Name config -Value Dotfiles-Bare-Git
+$env:PATH += ";C:\Program Files\Git\bin"
